6edffa14523785ed18ef4870d6ca3dc3dd0b453e
Alex Dodge <alexdodge+fcc@gmail.com>
2024-09-29 20:31:11 -0700

    Complete assignment

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require("body-parser");
 const cors = require('cors');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,9 +17,29 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const registry = new Map();
+
+app.post("/api/shorturl", (req,res) => {
+  const {url} = req.body;
+  try {
+    new URL(url)
+  }catch{
+    res.json({error: "invalid url"});
+    return;
+  }
+
+  const next_id = registry.size;
+  registry.set(next_id, url);
+
+  res.json({
+    original_url: url,
+    short_url: next_id,
+  })
+});
+
+app.get("/api/shorturl/:short", (req,res) => {
+  const url = registry.get(Number(req.params.short));
+  res.redirect(url);
 });
 
 app.listen(port, function() {
